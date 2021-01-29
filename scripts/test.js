8b46d70e11d09198ad419a939dc5c3d84846265d
sorensej <61718819+sorensej@users.noreply.github.com>
2021-01-28 20:53:31 -0500

    added some code to display stuff

--- scripts/test.js
+++ scripts/test.js
@@ -1,5 +1,5 @@
-username = ''; // Needs "Guest" account for viewing
-pass = '';
+username = 'sorensej@rose-hulman.edu'; // Needs "Guest" account for viewing
+pass = 'Flyingfox144';
 
 
 
@@ -10,24 +10,24 @@ pass = '';
 
 
 
-var Connection = require('tedious').Connection;  
-var config = {  
-    server: 'titan.csse.rose-hulman.edu',  //update me
+var Connection = require('tedious').Connection;
+var config = {
+    server: 'titan.csse.rose-hulman.edu', //update me
     authentication: {
         type: 'default',
         options: {
             userName: `${username}`, //update me
-            password: `${pass}`  //update me
+            password: `${pass}` //update me
         }
     },
     options: {
         // If you are on Microsoft Azure, you need encryption:
         encrypt: false,
-        database: 'ClubGearLocker_S1G5_jacksob1_buczkob1_sorensej'  //update me
+        database: 'ClubGearLocker_S1G5_jacksob1_buczkob1_sorensej' //update me
     }
-};  
-var connection = new Connection(config);  
-connection.on('connect', function(err) {  
+};
+var connection = new Connection(config);
+connection.on('connect', function (err) {
     // If no error, then good to proceed.
     if (err) {
         console.log(err);
@@ -42,31 +42,48 @@ var Request = require('tedious').Request;
 var Types = require('tedious').TYPES;
 
 function executeStatement() {
-    request = new Request("SELECT * FROM Item;", function(err) {  
-        if (err) {  
-            console.log(err);}  
-        });  
-        var result = "";  
-        request.on('row', function(columns) {  
-            columns.forEach(function(column) {  
-              if (column.value === null) {  
-                console.log('NULL');  
-              } else {  
-                result+= column.value + "\t\t";  
-              }  
-            });  
-            console.log(result);  
-            result ="";  
-        });  
-  
-        request.on('doneInProc', function(rowCount, more) {  
-            console.log(rowCount + ' rows returned');
-        });  
-
-        request.on('requestCompleted', function() {
-            connection.close();
+    request = new Request("SELECT * FROM Item;", function (err) {
+        if (err) {
+            console.log(err);
+        }
+    });
+    var result = "";
+    $("#rentalsContainer").innerHTML = "";
+    const newList = htmlToElement('<div id="rentalsContainer" class="col-md-8"></div>');
+    request.on('row', function (columns) {
+        columns.forEach(function (column) {
+            if (column.value === null) {
+                console.log('NULL');
+            } else {
+                result += column.value + "\t\t";
+                let template = document.querySelector("#cardTemplate");
+                let newCard = template.content.cloneNode(true);
+                newCard.querySelector(".inventory-listitem").innerHTML = column.value;
+                let interiorTemplate = document.querySelector("#listItemInterior");
+                //let interiorCard = interiorTemplate.content.cloneNode(true);
+                //interiorCard.querySelector(".item-pic").src = doc.data().src;
+                newCard.querySelector(".inventory-listitem").append(interiorCard);
+                newList.append(newCard);
+            }
         });
+        console.log(result);
 
-        connection.execSql(request);
+        //removes the old rentalsContainer and id
+        const oldList = document.querySelector("#rentalsContainer");
+        oldList.removeAttribute("id");
+        oldList.hidden = true;
+        oldList.parentElement.appendChild(newList);
+        result = "";
+    });
+
+    request.on('doneInProc', function (rowCount, more) {
+        console.log(rowCount + ' rows returned');
+    });
+
+    request.on('requestCompleted', function () {
+        connection.close();
+    });
+
+    connection.execSql(request);
 }
 connection.connect();
