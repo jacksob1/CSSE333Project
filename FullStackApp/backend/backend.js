80d2657b8f8e70371f94bf81c59ac789a1804044
buczkob1 <buczkob1@rose-hulman.edu>
2021-02-02 21:15:22 -0500

    Got info from database in localhost:3000/db/. Username and password need to be filled in before it can run.

--- FullStackApp/backend/backend.js
+++ FullStackApp/backend/backend.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var app = express();
+var fs = require("fs");
 var cors = require("cors");
 app.use(cors());
 
@@ -136,5 +137,101 @@ app.get("/api/id/:id", function (req, res) {
     // res.end();
 });
 
+app.use('/static', express.static("public"));
+
+app.get("/main", function(req, res) {
+    res.send("<h1>Goodbye!<h1>");
+});
+
+app.get("/db", function(req, res) {
+    username = ''; // Needs "Guest" account for viewing
+pass = '';
+
+
+
+
+
+
+
+
+
+
+var Connection = require('tedious').Connection;  
+var config = {  
+    server: 'titan.csse.rose-hulman.edu',  //update me
+    authentication: {
+        type: 'default',
+        options: {
+            userName: `${username}`, //update me
+            password: `${pass}`  //update me
+        }
+    },
+    options: {
+        // If you are on Microsoft Azure, you need encryption:
+        encrypt: false,
+        database: 'ClubGearLocker_S1G5_jacksob1_buczkob1_sorensej'  //update me
+    }
+};  
+var connection = new Connection(config);  
+connection.on('connect', function(err) {  
+    // If no error, then good to proceed.
+    if (err) {
+        console.log(err);
+        process.exit(1);
+    }
+    console.log("Connected");
+    executeStatement();
+    return;
+});
+
+var Request = require('tedious').Request;
+var Types = require('tedious').TYPES;
+
+let data = [];
+
+function executeStatement() {
+    request = new Request("SELECT * FROM Item;", function(err) {  
+        if (err) {  
+            console.log(err);}  
+        });  
+        request.on('row', function(columns) {     
+            let arr = [];
+            columns.forEach(function(column) {
+                let value = "";
+              if (column.value === null) {  
+                value += 'NULL';  
+              } else {  
+                value += column.value + "";  
+              }
+              arr.push(value);
+            });
+            data.push(arr);
+        });  
+  
+        request.on('doneInProc', function(rowCount, more) {
+            console.log(rowCount + ' rows returned');
+        });  
+
+        request.on('requestCompleted', function() {
+            connection.close();
+            let stringThing = "";
+            let num = 1;
+            data.forEach(function(row) {
+                stringThing += "<ul>\n";
+                stringThing += "<ol>Row "+num+"</ol>\n";
+                row.forEach(function(col) {
+                    stringThing += "<ol>"+col+"</ol>\n";
+                });
+                stringThing += "</ul>\n";
+                stringThing += "<br>";
+                num++;
+            });
+            res.send(stringThing);
+        });
+
+        connection.execSql(request);
+}
+connection.connect();
+})
 
 app.listen(3000);
