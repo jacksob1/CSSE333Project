3ef9982d6a807ada3cba40779e6312260c578824
sorensej <61718819+sorensej@users.noreply.github.com>
2021-02-04 13:31:46 -0500

    Fixed the html of the search page, added comments.

--- backend/backend.js
+++ backend/backend.js
@@ -12,27 +12,29 @@ app.get("/main", function (req, res) {
     res.send("<h1>Goodbye!<h1>");
 });
 
+//make the configuration to send to the connection
 function makeConfig() {
     username = 'club_gear_application_account'; // Needs "Guest" account for viewing
     pass = 'csse333w2021';
     var config = {
-        server: 'titan.csse.rose-hulman.edu', //update me
+        server: 'titan.csse.rose-hulman.edu',
         authentication: {
             type: 'default',
             options: {
-                userName: `${username}`, //update me
-                password: `${pass}` //update me
+                userName: `${username}`,
+                password: `${pass}` 
             }
         },
         options: {
             // If you are on Microsoft Azure, you need encryption:
             encrypt: false,
-            database: 'ClubGearLocker_S1G5_jacksob1_buczkob1_sorensej' //update me
+            database: 'ClubGearLocker_S1G5_jacksob1_buczkob1_sorensej'
         }
     };
     return config;
 }
 
+//get all values from the items table
 app.get("/db", function (req, res) {
     var Connection = require('tedious').Connection;
     var config = makeConfig();
@@ -50,15 +52,45 @@ app.get("/db", function (req, res) {
     connection.connect();
 })
 
+//search for items with a search word parameter
+app.get("/search/:search", function (req, res) {
+    //retrieve the search word from the parameters
+    let searchword = req.params.search;
+    //set to empty string if only default provided
+    if (searchword == "DEFAULT_SEARCH_PARAM") {
+        searchword = "";
+    }
+    console.log(searchword);
+    //make connection and config
+    var Connection = require('tedious').Connection;
+    var config = makeConfig();
+    var connection = new Connection(config);
+    connection.on('connect', function (err) {
+        // If no error, then good to proceed.
+        if (err) {
+            console.log(err);
+            process.exit(1);
+        }
+        console.log("Connected");
+        //execute statement after the connection
+        executeStatement(res, connection, "SELECT * FROM Item WHERE Item.Name LIKE '%" + searchword + "%';");
+        return;
+    });
+    connection.connect();
+})
 
+//execute the the search statement and send a response using res and connection
 function executeStatement(res, connection, searchStatement) {
     let data = [];
+    //create the request using the search statement
     var Request = require('tedious').Request;
     request = new Request(searchStatement, function (err) {
         if (err) {
             console.log(err);
         }
     });
+
+    //make an array of the columns
     request.on('row', function (columns) {
         let arr = [];
         columns.forEach(function (column) {
@@ -79,33 +111,12 @@ function executeStatement(res, connection, searchStatement) {
 
     request.on('requestCompleted', function () {
         connection.close();
+        //return the requested data
         res.send(data);
     });
-
+    //execute the request
     connection.execSql(request);
 }
 
-
-app.get("/search/:search", function (req, res) {
-    let searchword = req.params.search;
-    if (searchword == "DEFAULT_SEARCH_PARAM") {
-        searchword = "";
-    }
-    console.log(searchword);
-    var Connection = require('tedious').Connection;
-    var config = makeConfig();
-    var connection = new Connection(config);
-    connection.on('connect', function (err) {
-        // If no error, then good to proceed.
-        if (err) {
-            console.log(err);
-            process.exit(1);
-        }
-        console.log("Connected");
-        executeStatement(res, connection, "SELECT * FROM Item WHERE Item.Name LIKE '%" + searchword + "%';");
-        return;
-    });
-    connection.connect();
-})
-
+//listen for any requests
 app.listen(4000);
--- frontend/public/scripts/main.js
+++ frontend/public/scripts/main.js
@@ -1,10 +1,5 @@
-//const { response } = require("express");
-
 apiURL = "http://localhost:4000/search/";
-var selectedId = "";
 var defaultSearchword = "DEFAULT_SEARCH_PARAM";
-
-
 
 //From https://stackoverflow.com/questions/494143/creating-a-new-dom-element-from-an-html-string-using-built-in-dom-methods-or-pro/35385518#35385518
 function htmlToElement(html) {
@@ -19,10 +14,8 @@ function main() {
 
     loadEntries(""); //get data and populate entries
 
+    //if enter is pressed in the searchbar, send a request for the searchword
     $(".search-container").keyup(function (event) {
-        // if ($(".search-container").is(':focus')&& event.which === 13) {​​
-        //     alert("hello");
-        // }​​
         if (event.which === 13){
             let searchword = document.querySelector("#search").value;
             console.log(searchword);
@@ -31,32 +24,41 @@ function main() {
     });
 }
 
-
-
+//needs to be done (probably)
 function updateView() {
-
 }
+
 
 function loadEntries(string) {
     console.log("got here\n");
+    //if the search word is empty, use the default string
     if (string == ""){
         string = defaultSearchword;
     }
+    //empty the rentals container
     $("#rentalsContainer").innerHTML = "";
+    //create a new replacement for the table
     const newList = htmlToElement('<div id="rentalsContainer" class="col-md-8"></div>');
+    //request the items with the URL and search word
     fetch(apiURL+string).then(
         response => response.json()
     ).then((data) => {
         console.log("response??\n");
         console.log(data);
+        //loop through the data entering information to the card
         for (var i = 0; i < data.length; i++) {
+            //select and create templates
             let template = document.querySelector("#cardTemplate");
             let newCard = template.content.cloneNode(true);
             let interiorTemplate = document.querySelector("#listItemInterior");
+            //put the item name in the newCard
             newCard.querySelector(".inventory-listitem").innerHTML = data[i][3];
+            //define the interior structure for the description and set it to the item's description
             let interiorCard = interiorTemplate.content.cloneNode(true);
             interiorCard.querySelector(".description").innerHTML = data[i][4];
+            //appent the interior card to the list item
             newCard.querySelector(".inventory-listitem").append(interiorCard);
+            //add the card to the new list
             newList.append(newCard);
         }
         console.log("finished");
@@ -64,83 +66,102 @@ function loadEntries(string) {
     //removes the old rentalsContainer and id
     const oldList = document.querySelector("#rentalsContainer");
     oldList.removeAttribute("id");
+    //hide the old container and replace with the new list
     oldList.hidden = true;
     oldList.parentElement.appendChild(newList);
 }
 
-function loadEntry(id) {
-    selectedId = id;
-    let entry = fetch(apiURL + "id/" + id).then(
-        response => response.json()).then(data => {
-        document.querySelector("#inputName").value = data.name;
-        counter = data.count;
-        editEntryMode = true;
-        updateView();
-    });
+//Fix scrolling issues: https://www.w3schools.com/howto/howto_js_sticky_header.asp
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {scrollFct()};
+
+// Get the header
+var header = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function scrollFct() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
 }
 
+// we have not done this part yet
+// function loadEntry(id) {
+//     selectedId = id;
+//     let entry = fetch(apiURL + "id/" + id).then(
+//         response => response.json()).then(data => {
+//         document.querySelector("#inputName").value = data.name;
+//         counter = data.count;
+//         editEntryMode = true;
+//         updateView();
+//     });
+// }
 
-function createEntry() {
-    let name = document.querySelector("#inputName").value;
-    let data = {
-        "name": name,
-        "count": counter
-    };
+// function createEntry() {
+//     let name = document.querySelector("#inputName").value;
+//     let data = {
+//         "name": name,
+//         "count": counter
+//     };
 
-    let entry = fetch(apiURL, {
-        method: "POST",
-        headers: {
-            "Content-Type": 'application/json'
-        },
-        body: JSON.stringify(data)
-    }).then((data) => {
-        editEntryMode = false;
-        document.querySelector("#inputName").value = "";
-        counter = 0;
-        updateView();
-        loadEntries();
-    }).catch((err) => {
-        console.log(err);
-    });
-}
+//     let entry = fetch(apiURL, {
+//         method: "POST",
+//         headers: {
+//             "Content-Type": 'application/json'
+//         },
+//         body: JSON.stringify(data)
+//     }).then((data) => {
+//         editEntryMode = false;
+//         document.querySelector("#inputName").value = "";
+//         counter = 0;
+//         updateView();
+//         loadEntries();
+//     }).catch((err) => {
+//         console.log(err);
+//     });
+// }
 
-function deleteEntry() {
-    fetch(apiURL + "id/" + selectedId, {
-        method: "DELETE"
-    }).then(data => {
-        editEntryMode = false;
-        document.querySelector("#inputName").value = "";
-        counter = 0;
-        updateView();
-        loadEntries();
-    }).catch((err) => {
-        console.log(err);
-    });
-}
+// function deleteEntry() {
+//     fetch(apiURL + "id/" + selectedId, {
+//         method: "DELETE"
+//     }).then(data => {
+//         editEntryMode = false;
+//         document.querySelector("#inputName").value = "";
+//         counter = 0;
+//         updateView();
+//         loadEntries();
+//     }).catch((err) => {
+//         console.log(err);
+//     });
+// }
 
 
-function updateEntry() {
-    let name = document.querySelector("#inputName").value;
-    let data = {
-        "name": name,
-        "count": counter
-    };
-    fetch(apiURL + "id/" + selectedId, {
-        method: "PUT",
-        headers: {
-            "Content-Type": 'application/json'
-        },
-        body: JSON.stringify(data)
-    }).then(data => {
-        editEntryMode = false;
-        document.querySelector("#inputName").value = "";
-        counter = 0;
-        updateView();
-        loadEntries();
-    }).catch((err) => {
-        console.log(err);
-    });
-}
-
+// function updateEntry() {
+//     let name = document.querySelector("#inputName").value;
+//     let data = {
+//         "name": name,
+//         "count": counter
+//     };
+//     fetch(apiURL + "id/" + selectedId, {
+//         method: "PUT",
+//         headers: {
+//             "Content-Type": 'application/json'
+//         },
+//         body: JSON.stringify(data)
+//     }).then(data => {
+//         editEntryMode = false;
+//         document.querySelector("#inputName").value = "";
+//         counter = 0;
+//         updateView();
+//         loadEntries();
+//     }).catch((err) => {
+//         console.log(err);
+//     });
+// }
 
 main();
